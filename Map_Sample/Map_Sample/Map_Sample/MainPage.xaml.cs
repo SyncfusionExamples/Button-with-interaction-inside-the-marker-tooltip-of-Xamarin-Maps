4e06aff1f603adbb2fc6f51decaee39a024976e3
Vignesh Ramesh <vignesh.ramesh@syncfusion.com>
2022-01-19 12:50:56 +0530

    Sample added.

---
+++ Map_Sample/Map_Sample/Map_Sample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Map_Sample
+{
+    using Syncfusion.SfMaps.XForms;
+    using System;
+    using System.ComponentModel;
+    using Xamarin.Forms;
+
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Clicked(object sender, EventArgs e)
+        {
+            // It will be triggered when click the OK button.
+        }
+
+        private void Cancel_Clicked(object sender, EventArgs e)
+        {
+            // It will be triggered when click the Cancel button.
+        }
+    }
+
+    public class CustomMarker: MapMarker
+    {
+        public string Country { get; set; }
+
+        public string Population { get; set; }
+    }
+}
